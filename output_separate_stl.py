19456c527f4ef429562575276d1d1af662afd0f6
TakaoWing <tt.wing2838783@gmail.com>
2021-07-22 01:12:51 +0900

    Update output_separate_stl.py

--- output_separate_stl.py
+++ output_separate_stl.py
@@ -1,12 +1,17 @@
 import bpy
 from pathlib import Path
+import sys
 
 for item in bpy.data.objects:
     bpy.data.objects.remove(item)
 
-bpy.ops.import_mesh.stl(filepath="input.stl")
+bpy.ops.import_mesh.stl(filepath="/content/input.stl")
+
+bpy.ops.object.select_all(action='SELECT')
 
 bpy.ops.mesh.separate(type="LOOSE")
+
+bpy.ops.object.select_all(action='DESELECT')
 
 context = bpy.context
 scene = context.scene
@@ -15,11 +20,11 @@ viewlayer = context.view_layer
 
 obs = [o for o in scene.objects if o.type == 'MESH']   
 
-path = Path("/Users/tsubasatakaoka/Desktop/")
+path = Path("/content/output/")
 for ob in obs:
     viewlayer.objects.active = ob
     ob.select_set(True)
-    stl_path = path / f"output_{ob.name}.stl"
+    stl_path = path / f"{ob.name}.stl"
     bpy.ops.export_mesh.stl(
             filepath=str(stl_path),
             use_selection=True)
