680a07e261c732bdc1eac64488e1e1bd2d2637a5
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2021-07-22 10:43:51 +0900

    Update output_separate_stl.py

--- output_separate_stl.py
+++ output_separate_stl.py
@@ -5,7 +5,7 @@ import sys
 for item in bpy.data.objects:
     bpy.data.objects.remove(item)
 
-bpy.ops.import_mesh.stl(filepath="/content/input.stl")
+bpy.ops.import_mesh.stl(filepath=sys.argv[1])
 
 bpy.ops.object.select_all(action='SELECT')
 
